29c58af8b3d024649ca4f367f5377b37d517f1d4
Ruben Giaquinta <ruben.giaquinta@gmail.com>
2015-10-20 11:07:26 +0100

    added output data stream

--- KmeansClustering/src/Test.java
+++ KmeansClustering/src/Test.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 
 
@@ -6,10 +10,21 @@ public class Test {
 	public static void main(String[] args) {
 		
 		Kmeans km=new Kmeans("data.txt",5);
-		LinkedList<double[]> centroids2=km.update();
-		for(int i=0; i<centroids2.size();i++){
-			System.out.print(centroids2.get(i)[0]+",");
-			System.out.print(centroids2.get(i)[1]+"\n");
+		LinkedList<double[]> centroids=km.update();
+		for(int i=0; i<centroids.size();i++){
+			System.out.print(centroids.get(i)[0]+",");
+			System.out.print(centroids.get(i)[1]+"\n");
+		}
+		
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new FileWriter("centroids.csv")));
+			for (int i = 0; i < centroids.size(); i++) {
+				out.write(centroids.get(i)[0]+","+centroids.get(i)[1]+"\n");
+			}
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
