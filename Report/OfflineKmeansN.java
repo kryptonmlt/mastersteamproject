591944bd8fb985abd783db0ba8fae76d90e1df48
Ruben Giaquinta <ruben.giaquinta@gmail.com>
2015-12-16 12:10:07 +0000

    no message

--- Report/OfflineKmeansN.java
+++ Report/OfflineKmeansN.java
@@ -13,7 +13,8 @@ public class OfflineKmeansN {
 	// the number of centroids
 
 	public OfflineKmeansN(String s, int k, int N) {
-
+		
+		//reading input files
 		String line = null;
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(s));
@@ -29,7 +30,7 @@ public class OfflineKmeansN {
 			for (int j = 0; j < data.size(); j++) {
 				pointsclusters.add(0);
 			}
-
+			//randomly initializing centroids
 			ArrayList<Integer> random = new ArrayList<Integer>();
 			for (int j = 0; j < k; j++) {
 				random.add(j);
@@ -61,11 +62,13 @@ public class OfflineKmeansN {
 				int maxIndex = -1;
 				for (int j = 0; j < Clusters.size(); j++) {
 					double powsum = 0;
+					//calculating Euclidean distances
 					for (int k = 0; k < data.get(0).length; k++) {
 						powsum += Math.pow(data.get(i)[k]
 								- Clusters.get(j).getCentroid()[k], 2);
 					}
 					double temp = Math.sqrt(powsum);
+					//searching for the smallest distance
 					if (maxIndex == -1 || temp <= max) {
 						max = temp;
 						maxIndex = j;
@@ -76,14 +79,16 @@ public class OfflineKmeansN {
 			}
 
 			for (int k = 0; k < Clusters.size(); k++) {
-
+				
+				//initializing centroids
 				double[] c_d = new double[data.get(0).length];
 				for (int j = 0; j < c_d.length; j++) {
 					c_d[j] = 0;
 				}
 
 				int points = Clusters.get(k).getPoints().size();
-
+				
+				//calculating the potential centroids updates
 				for (int i = 0; i < points; i++) {
 					for (int w = 0; w < c_d.length; w++) {
 						c_d[w] += Clusters.get(k).getPoints().get(i)[w];
@@ -97,27 +102,31 @@ public class OfflineKmeansN {
 				}
 
 				double[] conditions = new double[c_d.length];
-
+				
+				//calculating conditions: shift length
 				for (int w = 0; w < c_d.length; w++) {
 					conditions[w] = Math.abs(Clusters.get(k).getCentroid()[w]
 							- c_d[w]);
 				}
-
+				
 				int condcounter = 0;
+				//checking if conditions value are under convergence error
 				for(int w=0;w<conditions.length;w++){
-					if(conditions[w]<0.001)
+					if(conditions[w]<0.05)
+					//condcounter is increased each time a centroid is not eligible for further updates
 						condcounter++;
 				}
-
+				//if condcounter is equal to the number of centroids counter is incremented
 				if (condcounter == c_d.length) {
 					counter++;
 				} else {
 					for (int l = 0; l < c_d.length; l++) {
+						//updating centroids
 						Clusters.get(k).getCentroid()[l] = c_d[l];
 					}
 				}
 			}
-
+			//the program terminates if all centroids (Clusters.size()) are not eligible for further updates
 			if (counter == Clusters.size()) {
 				return Clusters;
 			}
---
+++ Report/images/Tools.java
@@ -0,0 +1,122 @@
+package org.masters.qge.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Tools {
+
+	private static Tools instance = null;
+	private Random r = null;
+
+	private Tools() {
+		r = new Random();
+	}
+
+	public static Tools getInstance() {
+		if (instance == null) {
+			instance = new Tools();
+		}
+		return instance;
+	}
+
+	/**
+	 * Get the average result of the data from that query
+	 * 
+	 * @param dataSet
+	 * @param query
+	 * @param theta
+	 *            search area next to query
+	 * @return
+	 */
+	public float[] getAverageDatumFromQuery(List<float[]> dataSet, float[] query, float theta) {
+		List<float[]> dataInTheta = new ArrayList<float[]>();
+		for (float[] d : dataSet) {
+			if (VectorFunctions.distance(query, d) < theta) {
+				dataInTheta.add(d);
+			}
+		}
+		return getAverage(dataInTheta);
+	}
+
+	/**
+	 * Get average of a list of points
+	 * 
+	 * @param dataInTheta
+	 * @return
+	 */
+	public float[] getAverage(List<float[]> dataInTheta) {
+		if (dataInTheta.isEmpty()) {
+			return null;
+		}
+		float[] avg = new float[dataInTheta.get(0).length];
+		for (float[] d : dataInTheta) {
+			for (int i = 0; i < d.length; i++) {
+				avg[i] += d[i];
+			}
+		}
+		for (int i = 0; i < avg.length; i++) {
+			avg[i] = (float) (Math.round((avg[i] / dataInTheta.size()) * 10000.0) / 10000.0);
+		}
+		return avg;
+	}
+
+	/**
+	 * Generate Query using given subspaces (lPoints)
+	 * 
+	 * @param lPoints
+	 *            if lPoints is null use whole space. Else use gaussian
+	 *            distribution on subspaces
+	 * @return Data
+	 */
+	public float[] generateQuery(List<float[]> lPoints, int noOfAxis) {
+
+		if (lPoints == null || lPoints.isEmpty()) {
+			float[] row = new float[noOfAxis];
+			for (int i = 0; i < row.length; i++) {
+				row[i] = Tools.getInstance().getRandom().nextFloat() - 0.5f;
+			}
+			return row;
+		}
+
+		int J = r.nextInt(lPoints.size()); // select random subspace
+		float[] input = lPoints.get(J);
+
+		float[] points = new float[input.length - 1];
+		float width = input[input.length - 1];
+		for (int i = 0; i < input.length - 1; i++) {
+			points[i] = input[i];
+		}
+		return this.getRandomPointInBox(points, width);
+	}
+
+	/**
+	 * Generate a random point in a box given center of box and width
+	 * 
+	 * @param point
+	 *            centre of box
+	 * @param width
+	 *            of box
+	 * @return
+	 */
+	private float[] getRandomPointInBox(float[] point, float width) {
+
+		float[] result = new float[point.length];
+		for (int i = 0; i < result.length; i++) {
+			float g = 0.0f;
+			boolean found = false;
+			while (!found) {
+				g = (float) ((r.nextGaussian() * (width / 3)) + point[i]);
+				if (g < (point[i] + width) && g > (point[i] - width)) {
+					found = true;
+				}
+			}
+			result[i] = g;
+		}
+		return result;
+	}
+
+	public Random getRandom() {
+		return r;
+	}
+}
